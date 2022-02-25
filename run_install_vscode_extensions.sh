7d44789db04d52597dc0586bec66bf1f7ffe0b1b
Jan Van Bruggen <JVanBruggen@itopia.com>
2022-02-24 19:54:24 -0700

    Add script to install VS Code extensions

---
+++ run_install_vscode_extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Installing VS Code extensions..."
+code --install-extension esbenp.prettier-vscode
+code --install-extension jdinhlife.gruvbox
+code --install-extension vscodevim.vim
+echo "Finished installing VS Code extensions."
