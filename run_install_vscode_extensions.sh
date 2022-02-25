6dac6afff22dbd2f97bd6e45f3c16e1a75d25859
Jan Van Bruggen <JVanBruggen@itopia.com>
2022-02-25 15:05:00 -0700

    Add script to install FiraCode font

--- run_install_vscode_extensions.sh
+++ run_install_vscode_extensions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Installing VS Code extensions..."
+
 code --install-extension esbenp.prettier-vscode
 code --install-extension jdinhlife.gruvbox
 code --install-extension vscodevim.vim
+
 echo "Finished installing VS Code extensions."
